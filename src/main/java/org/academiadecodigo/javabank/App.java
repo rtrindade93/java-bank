28a8f21477485898b388760d1474426d07a6254d
Ricardo Trindade <ricardopovoas.trindade@gmail.com>
2017-10-18 13:02:42 +0100

    spring working

--- src/main/java/org/academiadecodigo/javabank/App.java
+++ src/main/java/org/academiadecodigo/javabank/App.java
@@ -2,22 +2,8 @@ package org.academiadecodigo.javabank;
 
 import org.academiadecodigo.javabank.controller.Controller;
 import org.academiadecodigo.javabank.controller.LoginController;
-import org.academiadecodigo.javabank.persistence.H2WebServer;
-import org.academiadecodigo.javabank.persistence.SessionManager;
-import org.academiadecodigo.javabank.persistence.TransactionManager;
-import org.academiadecodigo.javabank.persistence.dao.jpa.JpaAccountDao;
-import org.academiadecodigo.javabank.persistence.dao.jpa.JpaCustomerDao;
-import org.academiadecodigo.javabank.persistence.jpa.JpaSessionManager;
-import org.academiadecodigo.javabank.persistence.jpa.JpaTransactionManager;
-import org.academiadecodigo.javabank.services.AccountServiceImpl;
-import org.academiadecodigo.javabank.services.AuthServiceImpl;
-import org.academiadecodigo.javabank.services.CustomerServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import java.sql.SQLException;
 
 public class App {
 
@@ -28,46 +14,5 @@ public class App {
         Controller controller = context.getBean("loginController", LoginController.class);
 
         controller.init();
-
-        /*try {
-
-            H2WebServer h2WebServer = new H2WebServer();
-            h2WebServer.start();
-
-            EntityManagerFactory emf = Persistence.createEntityManagerFactory(Config.PERSISTENCE_UNIT);
-            JpaSessionManager sm = new JpaSessionManager(emf);
-            TransactionManager tx = new JpaTransactionManager(sm);
-
-            App app = new App();
-            app.bootStrap(tx, sm);
-
-            emf.close();
-            h2WebServer.stop();
-
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }*/
     }
-
-    /*private void bootStrap(TransactionManager tx, JpaSessionManager sm) {
-
-        AccountServiceImpl accountService = new AccountServiceImpl();
-        accountService.setAccountDao(new JpaAccountDao(sm));
-        accountService.setTransactionManager(tx);
-
-        CustomerServiceImpl customerService = new CustomerServiceImpl();
-        customerService.setCustomerDao(new JpaCustomerDao(sm));
-        customerService.setTransactionManager(tx);
-
-        Bootstrap bootstrap = new Bootstrap();
-
-        bootstrap.setAuthService(new AuthServiceImpl());
-        bootstrap.setAccountService(accountService);
-        bootstrap.setCustomerService(customerService);
-
-        Controller controller = bootstrap.wireObjects();
-
-        // start application
-        controller.init();
-    }*/
 }
